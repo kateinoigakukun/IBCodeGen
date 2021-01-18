59ed3ff03b03d7d363864c5acfce43b6399876bb
Yuta Saito <kateinoigakukun@gmail.com>
2021-01-18 23:21:53 +0900

    Cleanup Assertion helper

---
+++ TestSuites/Sources/TestSuites/Assertion.swift
@@ -0,0 +1,54 @@
+//
+//  Assertion.swift
+//  TestSuites
+//
+//  Created by kateinoigakukun on 2021/01/18.
+//
+
+import Foundation
+import SnapshotTesting
+import XCTest
+
+func XCTAssertEqualSnapshot<Value, Format>(
+    _ lhs: Value, _ rhs: Value,
+    as snapshotting: Snapshotting<Value, Format>,
+    timeout: TimeInterval = 5, description: String = "",
+    file: StaticString = #file, line: UInt = #line
+) {
+    let tookSnapshot = XCTestExpectation(description: "Took snapshot")
+    var lhsDescription: Format!
+    var rhsDescription: Format!
+    snapshotting.snapshot(lhs).run { lhs in
+        snapshotting.snapshot(rhs).run { rhs in
+            lhsDescription = lhs
+            rhsDescription = rhs
+            tookSnapshot.fulfill()
+        }
+    }
+    guard let (failure, attachments) = snapshotting.diffing.diff(lhsDescription, rhsDescription) else {
+        return
+    }
+
+    if !attachments.isEmpty {
+        if ProcessInfo.processInfo.environment.keys.contains("__XCODE_BUILT_PRODUCTS_DIR_PATHS") {
+            XCTContext.runActivity(named: "Attached Failure Diff") { activity in
+                attachments.forEach {
+                    activity.add($0)
+                }
+            }
+        }
+    }
+    XCTFail("\(description) \(failure)", file: file, line: line)
+}
+func XCTAssertEqualProperties(_ lhs: UIView, _ rhs: UIView, timeout: TimeInterval = 5, description: String = "",
+                              file: StaticString = #file, line: UInt = #line) {
+    let snapshottings: [Snapshotting<UIView, String>] = [.recursiveDescription, .deepRecursiveDescription]
+    for snapshotting in snapshottings {
+        XCTAssertEqualSnapshot(lhs, rhs, as: snapshotting, timeout: timeout, description: description, file: file, line: line)
+    }
+}
+
+func XCTAssertEqualAppearance(_ lhs: UIView, _ rhs: UIView, timeout: TimeInterval = 5, description: String = "",
+                              file: StaticString = #file, line: UInt = #line) {
+    XCTAssertEqualSnapshot(lhs, rhs, as: .image, timeout: timeout, description: description, file: file, line: line)
+}
--- TestSuites/Sources/TestSuites/TestSuites.swift
+++ TestSuites/Sources/TestSuites/TestSuites.swift
@@ -1,56 +1,5 @@
 import XCTest
 import UIKit
-import SnapshotTesting
-
-func XCTAssertEqualProperties(_ lhs: UIView, _ rhs: UIView, timeout: TimeInterval = 5, description: String = "",
-                              file: StaticString = #file, line: UInt = #line) {
-    let snapshottings: [Snapshotting<UIView, String>] = [.recursiveDescription, .deepRecursiveDescription]
-    for snapshotting in snapshottings {
-        let tookSnapshot = XCTestExpectation(description: "Took snapshot")
-        var lhsDescription: String!
-        var rhsDescription: String!
-        snapshotting.snapshot(lhs).run { lhs in
-            snapshotting.snapshot(rhs).run { rhs in
-                lhsDescription = lhs
-                rhsDescription = rhs
-                tookSnapshot.fulfill()
-            }
-        }
-        guard let (failure, _) = snapshotting.diffing.diff(lhsDescription, rhsDescription) else {
-            return
-        }
-        XCTFail("\(description) \(failure)", file: file, line: line)
-    }
-}
-
-func XCTAssertEqualAppearance(_ lhs: UIView, _ rhs: UIView, timeout: TimeInterval = 5, description: String = "",
-                              file: StaticString = #file, line: UInt = #line) {
-    let snapshotting = Snapshotting<UIView, UIImage>.image
-    let tookSnapshot = XCTestExpectation(description: "Took snapshot")
-    var lhsDescription: UIImage!
-    var rhsDescription: UIImage!
-    snapshotting.snapshot(lhs).run { lhs in
-        snapshotting.snapshot(rhs).run { rhs in
-            lhsDescription = lhs
-            rhsDescription = rhs
-            tookSnapshot.fulfill()
-        }
-    }
-    guard let (failure, attachments) = snapshotting.diffing.diff(lhsDescription, rhsDescription) else {
-        return
-    }
-
-    if !attachments.isEmpty {
-        if ProcessInfo.processInfo.environment.keys.contains("__XCODE_BUILT_PRODUCTS_DIR_PATHS") {
-            XCTContext.runActivity(named: "Attached Failure Diff") { activity in
-                attachments.forEach {
-                    activity.add($0)
-                }
-            }
-        }
-    }
-    XCTFail("\(description) \(failure)", file: file, line: line)
-}
 
 final class IBCodeGenKitTests: XCTestCase {
     func testSimpleView() {
