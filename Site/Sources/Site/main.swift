735a0b73a426785cca5cde54ef300e3bbf8c0007
Yuta Saito <kateinoigakukun@gmail.com>
2021-01-28 00:06:18 +0900

    Add index.html

--- Site/Sources/Site/main.swift
+++ Site/Sources/Site/main.swift
@@ -1,39 +1,71 @@
 import CoreFoundation
-import FoundationXML
+#if canImport(FoundationXML)
+    import FoundationXML
+#endif
 import IBCodeGenKit
+import JavaScriptKit
+
 class ContentWriter: TextOutputStream {
     private(set) var content: String = ""
     func write(_ string: String) {
         content += string
     }
 }
+
 let generator = IBCodeGenerator()
-let xmlContent = """
-<?xml version="1.0" encoding="UTF-8"?>
-<document type="com.apple.InterfaceBuilder3.CocoaTouch.XIB" version="3.0" toolsVersion="17506" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" useTraitCollections="YES" useSafeAreas="YES" colorMatched="YES">
-    <device id="retina6_1" orientation="portrait" appearance="light"/>
-    <dependencies>
-        <deployment identifier="iOS"/>
-        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="17504.1"/>
-        <capability name="Safe area layout guides" minToolsVersion="9.0"/>
-        <capability name="documents saved in the Xcode 8 format" minToolsVersion="8.0"/>
-    </dependencies>
-    <objects>
-        <placeholder placeholderIdentifier="IBFilesOwner" id="-1" userLabel="File's Owner"/>
-        <placeholder placeholderIdentifier="IBFirstResponder" id="-2" customClass="UIResponder"/>
-        <view contentMode="scaleToFill" id="iN0-l3-epB">
-            <rect key="frame" x="0.0" y="0.0" width="414" height="896"/>
-            <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
-            <viewLayoutGuide key="safeArea" id="vUN-kp-3ea"/>
-            <color key="backgroundColor" red="1" green="1" blue="1" alpha="1" colorSpace="custom" customColorSpace="sRGB"/>
-            <point key="canvasLocation" x="139" y="149"/>
-        </view>
-    </objects>
-</document>
-"""
-var writer = ContentWriter()
-_ = try generator.generate(
-    from: xmlContent,
-    fileBaseName: "SimpleView",
-    target: &writer)
-print(writer.content)
+
+let document = JSObject.global.document
+let fileSelect = document.getElementById("ib-select").object!
+let dragBox = document.getElementById("drag-box").object!
+let codeArea = document.getElementById("code-area").object!
+
+let ace = JSObject.global.ace.object!
+let editor = ace.edit!(codeArea)
+_ = editor.session.setMode("ace/mode/swift")
+
+func handleFileSelect(_ file: JSValue) {
+    let fileName = file.name.string!
+    let baseName = String(fileName.split(separator: ".").dropLast().joined(separator: "."))
+
+    JSPromise(file.text().object!)!.then { value in
+        var writer = ContentWriter()
+        _ = try! generator.generate(
+            from: value.string!, fileBaseName: baseName,
+            target: &writer
+        )
+        _ = editor.setValue(JSValue.string(writer.content))
+        return JSValue.undefined
+    }
+}
+
+let fileSelectHandler = JSClosure { arguments in
+    let file = fileSelect.files.item(0)
+    handleFileSelect(file)
+    return .undefined
+}
+
+_ = fileSelect.addEventListener!("change", fileSelectHandler)
+
+let fileDragOver = JSClosure { args in
+    let event = args[0]
+    _ = event.preventDefault()
+    _ = dragBox.classList.add("border-yellow-300")
+    return .undefined
+}
+let fileDragLeave = JSClosure { args in
+    let event = args[0]
+    _ = event.preventDefault()
+    _ = dragBox.classList.remove("border-yellow-300")
+    return .undefined
+}
+let fileDrop = JSClosure { args in
+    let event = args[0]
+    _ = event.preventDefault()
+    _ = dragBox.classList.remove("border-yellow-300")
+    let file = event.dataTransfer.files.item(0)
+    handleFileSelect(file)
+    return .undefined
+}
+_ = dragBox.addEventListener!("dragover", fileDragOver)
+_ = dragBox.addEventListener!("dragleave", fileDragLeave)
+_ = dragBox.addEventListener!("drop", fileDrop)
