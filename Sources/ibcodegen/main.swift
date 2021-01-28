1aeb590db3d3c132f2b011fa90560a9d6176919a
Yuta Saito <kateinoigakukun@gmail.com>
2021-01-28 17:41:41 +0900

    Improve command help

--- Sources/ibcodegen/main.swift
+++ Sources/ibcodegen/main.swift
@@ -11,11 +11,14 @@ class ContentWriter: TextOutputStream {
 
 
 struct IBCodeGenTool: ParsableCommand {
-    @Argument(transform: URL.init(fileURLWithPath: ))
+    static let configuration = CommandConfiguration(
+        commandName: "ibcodegen", abstract: "Translate .xib or .storyboard into Swift"
+    )
+    @Argument(help: "An input .xib or .storyboard", transform: URL.init(fileURLWithPath: ))
     var input: URL
 
-    @Option(name: .short, transform: URL.init(fileURLWithPath: ))
-    var output: URL
+    @Option(name: .shortAndLong, help: "Write to file", transform: URL.init(fileURLWithPath: ))
+    var output: URL?
 
     func run() throws {
         let generator = IBCodeGenerator()
@@ -25,7 +28,11 @@ struct IBCodeGenTool: ParsableCommand {
             from: xmlContent,
             fileBaseName: input.deletingPathExtension().lastPathComponent,
             target: &writer)
-        try writer.content.write(to: output, atomically: true, encoding: .utf8)
+        if let output = output {
+            try writer.content.write(to: output, atomically: true, encoding: .utf8)
+        } else {
+            print(writer.content)
+        }
     }
 }
 
